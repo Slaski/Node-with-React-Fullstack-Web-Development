e1d9f7657122ddc3dc33e6e8de2abcb16ae51155
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-17 16:06:44 -0300

    feat: Add the User model.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport');
 const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
 
 const app = express();
 mongoose
