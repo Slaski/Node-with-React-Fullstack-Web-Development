2bf0c7f3b57d7f51645529e80f5c59b18aeaa8f1
Rodrigo Zanetta Slaski Suchorzewski <rsuchorzewski216@beijaflore.com>
2018-04-17 16:16:24 -0300

    feat: Callback passport when user is authenticated.

--- services/passport.js
+++ services/passport.js
@@ -13,10 +13,15 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(user => {
-        if (!user) {
-          // we don't have a record with the give profile ID
-          new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a record with the given profile ID
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
